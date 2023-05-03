78e3b63283cd64622bb17e6243e3a2ebe5db7dd4
userkrunal <120082312+userkrunal@users.noreply.github.com>
2023-05-03 17:47:02 +0530

    set

--- lib/home.dart
+++ lib/home.dart
@@ -18,14 +18,15 @@ class _Home_ScreenState extends State<Home_Screen> {
     providerT=Provider.of<StepProvider>(context,listen: true);
     return SafeArea(child: Scaffold(
       appBar: AppBar(
-        title: Text("Steper"),centerTitle: true,
+        backgroundColor: Colors.red,
+        title: Text("Stepper App"),centerTitle: true,
       ),
       body: Theme(
         data: ThemeData(
-          accentColor: Colors.orange,
-          primarySwatch: Colors.orange,
+          accentColor: Colors.red,
+          primarySwatch: Colors.red,
           colorScheme: ColorScheme.light(
-            primary: Colors.orange
+            primary: Colors.red
           )
         ),
         child: Stepper(
@@ -37,11 +38,56 @@ class _Home_ScreenState extends State<Home_Screen> {
             providerT!.close();
           },
           steps: [
-            Step(title: Text("Persoonal"), content: Text("Enter Your Name...")),
-            Step(title: Text("Document"), content: Text("Enter Document...")),
-            Step(title: Text("Documet verification"), content: Text("Verification Successful...")),
-            Step(title: Text("Payment Details"), content: Text("Enter Payment details...")),
-            Step(title: Text("Submit"), content: Text("Successful...")),
+            Step(title: Text("Signup"), content: Container(
+              height: 250,
+              width: 240,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("Full Name*"),
+                    leading: Icon(Icons.person,color: Colors.grey.shade400),
+
+                  ),
+                  Divider(color: Colors.black,height: 5,thickness: 2),
+                  ListTile(
+                    title: Text("Email-Id"),
+                    leading: Icon(Icons.mail,color: Colors.grey.shade400),
+                  ),
+                  Divider(color:Colors.black,height: 5,thickness: 2),
+                  ListTile(
+                    title: Text("Password*"),
+                    leading: Icon(Icons.lock,color: Colors.grey.shade400),
+                  ),
+                  Divider(color: Colors.black,height: 5,thickness: 2),
+                  ListTile(
+                    title: Text("Conform Password*"),
+                    leading: Icon(Icons.lock,color: Colors.grey.shade400),
+                  ),
+                  Divider(color: Colors.black,height: 5,thickness: 2),
+                ],
+              ),
+            )),
+            Step(title: Text("Login"), content: Container(
+              height: 130,
+              width: 240,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("User Name*"),
+                    leading: Icon(Icons.person,color: Colors.grey.shade400),
+                  ),
+                  Divider(color: Colors.black,height: 5,thickness: 2),
+                  ListTile(
+                    title: Text("Password"),
+                    leading: Icon(Icons.mail,color: Colors.grey.shade400),
+                  ),
+                  Divider(color: Colors.black,height: 5,thickness: 2),
+                ],
+              ),
+            )),
+            Step(title: Text("Home"),content: Text("Thank you")),
           ],
         ),
       ),
